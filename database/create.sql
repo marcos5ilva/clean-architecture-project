6db3f2a105493be9d82a0e995c587bc855fc0abc
Marcos Silva <msilva@hopefulinc.me>
2021-09-20 14:37:52 -0400

    refact: insert items into ccca.item table

--- database/create.sql
+++ database/create.sql
@@ -6,4 +6,8 @@ price numeric,
 height integer,
 width integer,
 length integer,
-weight interger)
+weight interger);
+
+insert into ccca.item (description, price, width, height, length, weight) values ('Guitarra', 1000, 100, 50, 15, 3);
+insert into ccca.item (description, price, width, height, length, weight) values ('Amplificador', 5000, 50, 50, 50, 22);
+insert into ccca.item (description, price, width, height, length, weight) values ('Cabo', 30, 10, 10, 10, 1);
